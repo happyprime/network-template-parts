92b2fed053a70bf6c6e6e8d17fdf08fc84b96feb
Jeremy Felt <jeremy.felt@gmail.com>
2023-04-13 11:30:51 -0700

    Add an actual plugin header

--- plugin.php
+++ plugin.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Plugin Name: Network Template Parts
- * Version: 0.1.0
+ * Plugin Name:  Network Template Parts
+ * Description:  Manage site and network level block template parts.
+ * Version:      0.1.0
+ * Plugin URI:   https://github.com/happyprime/network-template-parts/
+ * Author:       Happy Prime
+ * Author URI:   https://happyprime.co
+ * Text Domain:  network-template-parts
+ * Requires PHP: 7.4
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * @package network-template-parts
  */
