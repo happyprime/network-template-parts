b9b9ad4357c0b3c74fa114559e689b92aed3ad57
Jeremy Felt <jeremy.felt@gmail.com>
2023-07-12 14:08:47 -0700

    Include build files in the repo

---
+++ build/network-template-part/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ee39356c45eeb49652a4');
